3b8f2698bfd2555c21da0d326b7629d01d1c2296
queenrashu136-hue <queenrashu136@gmail.com>
2025-11-03 17:03:48 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -1,8 +1,8 @@
 import * as mega from 'megajs';
 
 const auth = {
-    email: 'lapico8670@evoxury.com', // Replace with your Mega email 
-    password: 'lapico8670@#2', // Replace with your Mega password
+    email: 'gipaf89766@fantastu.com', // Replace with your Mega email 
+    password: 'z.94FdFt-4KYjr8', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
